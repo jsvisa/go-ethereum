55afcadcf74c5732adeeafcf36d7ddf2d68ba988
jsvisa <delweng@gmail.com>
2025-04-08 14:36:19 +0000

    ethdb/pebble: async write
    Signed-off-by: jsvisa <delweng@gmail.com>

--- ethdb/pebble/pebble.go
+++ ethdb/pebble/pebble.go
@@ -215,7 +215,7 @@ func New(file string, cache int, handles int, namespace string, readonly bool, e
 		fn:           file,
 		log:          logger,
 		quitChan:     make(chan chan error),
-		writeOptions: &pebble.WriteOptions{Sync: !ephemeral},
+		writeOptions: &pebble.WriteOptions{Sync: false},
 	}
 
 	opt := &pebble.Options{
